2f526fe4ae5e245691d4abc062916311bbc956ae
Michal Miros <michalmiros7@wp.pl>
2022-11-01 19:07:10 +0100

    0.2 Creating TicTacToeField class

---
+++ src/main/java/com/game/tictactoe/TicTacToeField.java
@@ -0,0 +1,103 @@
+package com.game.tictactoe;
+
+public class TicTacToeField {
+    private char field1_1;
+    private char field1_2;
+    private char field1_3;
+    private char field2_1;
+    private char field2_2;
+    private char field2_3;
+    private char field3_1;
+    private char field3_2;
+    private char field3_3;
+
+    public TicTacToeField() {
+        field1_1 = ' ';
+        field1_2 = ' ';
+        field1_3 = ' ';
+        field2_1 = ' ';
+        field2_2 = ' ';
+        field2_3 = ' ';
+        field3_1 = ' ';
+        field3_2 = ' ';
+        field3_3 = ' ';
+    }
+
+    public void printField() {
+        System.out.println("|" + field1_1 + "|" + field1_2 + "|" + field1_3 + "|");
+        System.out.println("|" + field2_1 + "|" + field2_2 + "|" + field2_3 + "|");
+        System.out.println("|" + field3_1 + "|" + field3_2 + "|" + field3_3 + "|");
+    }
+
+    public char getField1_1() {
+        return field1_1;
+    }
+
+    public void setField1_1(char field1_1) {
+        this.field1_1 = field1_1;
+    }
+
+    public char getField1_2() {
+        return field1_2;
+    }
+
+    public void setField1_2(char field1_2) {
+        this.field1_2 = field1_2;
+    }
+
+    public char getField1_3() {
+        return field1_3;
+    }
+
+    public void setField1_3(char field1_3) {
+        this.field1_3 = field1_3;
+    }
+
+    public char getField2_1() {
+        return field2_1;
+    }
+
+    public void setField2_1(char field2_1) {
+        this.field2_1 = field2_1;
+    }
+
+    public char getField2_2() {
+        return field2_2;
+    }
+
+    public void setField2_2(char field2_2) {
+        this.field2_2 = field2_2;
+    }
+
+    public char getField2_3() {
+        return field2_3;
+    }
+
+    public void setField2_3(char field2_3) {
+        this.field2_3 = field2_3;
+    }
+
+    public char getField3_1() {
+        return field3_1;
+    }
+
+    public void setField3_1(char field3_1) {
+        this.field3_1 = field3_1;
+    }
+
+    public char getField3_2() {
+        return field3_2;
+    }
+
+    public void setField3_2(char field3_2) {
+        this.field3_2 = field3_2;
+    }
+
+    public char getField3_3() {
+        return field3_3;
+    }
+
+    public void setField3_3(char field3_3) {
+        this.field3_3 = field3_3;
+    }
+}
--- src/main/java/com/game/tictactoe/TicTacToeRunner.java
+++ src/main/java/com/game/tictactoe/TicTacToeRunner.java
@@ -2,6 +2,7 @@ package com.game.tictactoe;
 
 public class TicTacToeRunner {
     public static void main(String[] args) {
-        //
+        TicTacToeField field = new TicTacToeField();
+        field.printField();
     }
 }
